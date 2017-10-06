f520b4c1ffa66507583313fd88ecfb34a7daf223
05230006 <05230006@D98923.farroupilha.ifrs.edu.br>
2017-10-06 20:59:15 -0300

    Foi

--- src/estacionamento/Estacionamento.java
+++ src/estacionamento/Estacionamento.java
@@ -22,7 +22,7 @@ public class Estacionamento extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
-        
+        //test
         stage.setScene(scene);
         stage.show();
     }
