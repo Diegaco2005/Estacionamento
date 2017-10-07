c7a683ee67708a7f96b3c69750105346c4ca9e34
Diego <Diego@192.168.56.1>
2017-10-06 21:03:28 -0300

    Teste de commit de novo arquivo

--- src/estacionamento/Estacionamento.java
+++ src/estacionamento/Estacionamento.java
@@ -20,7 +20,7 @@ public class Estacionamento extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
+        //Diego
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
